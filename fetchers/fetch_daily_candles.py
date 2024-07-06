c87ffe35166b8fd7767f9ad2949305ed582b0847
thegicode <thegi.code@gmail.com>
2024-07-06 19:57:54 +0900

    update fetch_and_save_daily_candles: update last date data

--- fetchers/fetch_daily_candles.py
+++ fetchers/fetch_daily_candles.py
@@ -18,7 +18,7 @@ async def fetch_and_save_daily_candles(markets, count):
 
     for market in markets:
         save_path = os.path.join(save_dir, f"daily_candles_{market}.csv")
-        print(f"Fetching daily candles for {market}...")
+        print(f"\nFetching daily candles for {market}...")
 
         now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
         end = now.replace(hour=9, minute=0, second=0, microsecond=0)
@@ -28,24 +28,22 @@ async def fetch_and_save_daily_candles(markets, count):
         if os.path.exists(save_path):
             existing_data = pd.read_csv(save_path, index_col=0, parse_dates=True)
             last_date = existing_data.index[-1]
+
             if last_date.tzinfo is None:
                 last_date = last_date.tz_localize('Asia/Seoul')
             else:
                 last_date = last_date.tz_convert('Asia/Seoul')
 
-            if last_date >= end:
-                print(f"Data for {market} is already up to date.")
-                continue
-
-            start = last_date + datetime.timedelta(days=1)
+            start = last_date  # 마지막 날짜부터 다시 시작
             df_list.append(existing_data)
 
-        while start < end:
+        while start <= end:
             try:
                 df = get_daily_candles(market, min(count, (end - start).days + 1))
                 df = df.sort_index()  # 인덱스를 기준으로 오래된 시간 순으로 정렬
+                print(df)
                 df_list.append(df)
-                start += datetime.timedelta(days=min(count, (end - start).days))
+                start += datetime.timedelta(days=len(df))
                 await asyncio.sleep(1)  # 요청 간 지연 시간 추가
             except HTTPError as http_err:
                 if http_err.response.status_code == 429:
