f640cb4c5952eeb81848c36b33433d0fcec16b56
thegicode <thegi.code@gmail.com>
2024-08-05 19:47:42 +0900

    udpate daily_enhance

--- backtest/bianace/daily_enhanced.py
+++ backtest/bianace/daily_enhanced.py
@@ -14,7 +14,7 @@ def calculate_moving_average(df, window):
     df[f'{window}MA'] = df['Close'].rolling(window=window).mean()
 
 # 매매 전략 시뮬레이션
-def simulate_moving_average_trading(df, window=40, initial_capital=500000, fee_rate=0.001):
+def simulate_moving_average_trading(df, window=40, initial_capital=1000000, fee_rate=0.001):
     ma_column = f'{window}MA'
     in_position = False
     trades = []
@@ -34,13 +34,14 @@ def simulate_moving_average_trading(df, window=40, initial_capital=500000, fee_r
             trades.append(('buy', df['Close Time'][i], buy_price, capital, investment, None, None, df[ma_column][i]))
             in_position = True
 
-        # 상승장에서 1만원 추가 매수 (40일 조건에 들지 않았을 때만)
+        # 상승장에서 추가 매수 (40일 조건에 들지 않았을 때만)
         elif not in_position and df['Close'][i] > df['Close'][i-1] and df['Volume'][i] > df['Volume'][i-1]:
-            # 추가 매수: 상승장(가격과 거래량이 증가)일 때 10,000원어치 매수
+            # 추가 매수: 상승장(가격과 거래량이 증가)일 때 남은 자본금의 1%만큼 매수
+            additional_investment_amount = capital * 0.01  # 남은 자본금의 1%
             additional_buy_price = df['Close'][i] * (1 + fee_rate)
-            if capital >= 10000:
-                additional_quantity = 10000 / additional_buy_price
-                investment += 10000  # 추가 매수에 따른 투자금 증가
+            if capital >= additional_investment_amount:
+                additional_quantity = additional_investment_amount / additional_buy_price
+                investment += additional_investment_amount  # 추가 매수에 따른 투자금 증가
                 capital -= additional_buy_price * additional_quantity
                 quantity += additional_quantity
                 trades.append(('additional_buy', df['Close Time'][i], additional_buy_price, capital, investment, None, None, df[ma_column][i]))
@@ -62,11 +63,11 @@ def simulate_moving_average_trading(df, window=40, initial_capital=500000, fee_r
     return trades
 
 # 성과 지표 계산
-def calculate_performance(trades, initial_capital=500000):
+def calculate_performance(trades, initial_capital=1000000):
     if not trades:
         return {}
 
-    profits = [trade[5] for trade in trades if trade[0] == 'sell']  # profit 인덱스 수정
+    profits = [trade[5] for trade in trades if trade[0] == 'sell']
     total_returns = sum(profits)
     percent_return = (total_returns / initial_capital) * 100
     win_rate = (len([p for p in profits if p > 0]) / len(profits)) * 100 if profits else 0
@@ -146,7 +147,7 @@ def run_backtest(file_path, initial_capital):
 
 # 파일 경로, 초기값
 file_path = 'data/binance/daily_candles_BTCUSDT.csv'
-initial_capital = 4000000  # 초기 자본금 40만원
+initial_capital = 1000000  # 초기 자본금 100만원
 
 # 결과 출력 및 저장
 backtest_results = run_backtest(file_path, initial_capital)
